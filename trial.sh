f6f84a9d2b713028d7d4515a922388ac58ef6240
AidikaAA <mitrassh10@gmail.com>
2017-07-10 07:47:06 +0700

    Create trial.sh

---
+++ trial.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Script Copyright www.fornesia.com
+# Mod by Bustami Arifin a.k.a Abu Yazid Al-Busthami
+# ==================================================
+# 
+
+
+
+        clear
+        echo -e "+============================================================================+" | lolcat
+        echo -e "+                                                                            +" | lolcat
+        echo -e "+                       Script Premium Modified by DikaNET                   +" | lolcat 
+        echo -e "+                             <<= Contact Person =>>                         +" | lolcat
+        echo -e "+                        BBM :D69F79CA | WA :082228644803                    +" | lolcat
+        echo -e "+                                                                            +" | lolcat
+        echo -e "+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=++=+=+=+=+=+=+" | lolcat
+        echo -e "------------------------------------------------------------------------------" | lolcat
+        echo -e ">>>>>>>>>>                      Generate Akun SSH                   <<<<<<<<<<" | lolcat
+        echo -e "------------------------------------------------------------------------------" | lolcat
+        echo -e " "
+        echo -e " "
+	uname=trial-`</dev/urandom tr -dc X-Z0-9 | head -c4`
+	masaaktif="1"
+	pass=`</dev/urandom tr -dc a-f0-9 | head -c9`
+	clear
+	create_user
+	break
+	;;
