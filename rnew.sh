76acf0b9c075d47450cc62c67e9fdda57c595525
AidikaAA <mitrassh10@gmail.com>
2017-07-10 14:09:03 +0700

    Update rnew.sh

--- rnew.sh
+++ rnew.sh
@@ -6,6 +6,23 @@
 # 
 
 
-
-echo "Kadaluarsa User: $uname Di Perbarui Sampai: $expdate"| lolcat;
-usermod -e $expdate $uname
+            clear
+	          echo -e " "
+            echo -e " "
+	          echo -e "+============================================================================+" | lolcat
+            echo -e "+                                                                            +" | lolcat
+            echo -e "+                       Script Premium Modified by DikaNET                   +" | lolcat 
+            echo -e "+                             <<= Contact Person =>>                         +" | lolcat
+            echo -e "+                        BBM :D69F79CA | WA :082228644803                    +" | lolcat
+            echo -e "+                                                                            +" | lolcat
+            echo -e "+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=++=+=+=+=+=+=+" | lolcat
+            echo -e "-------------------------------------------------------------------------------" | lolcat
+            echo -e ">>>>>>>>>>                        Renew Akun SSH/OVPN                <<<<<<<<<<" | lolcat
+            echo -e "-------------------------------------------------------------------------------" | lolcat
+            echo -e " "
+            echo -e " "
+            read -p "Enter username yg di perbarui: " uname
+            echo "Kadaluarsa User: $uname Di Perbarui Sampai: $expdate"| lolcat;
+            read -p "Aktif sampai tanggal Thn-Bln-Hr(YYYY-MM-DD): " expdate
+            usermod -e $expdate $uname
+            
